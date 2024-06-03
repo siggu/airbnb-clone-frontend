167f39768220c0665fad2de4113ae728e46b63a9
siggu <106001755+siggu@users.noreply.github.com>
2024-06-03 11:30:28 +0900

    Update SocialLogin.tsx

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -1,25 +1,25 @@
-import { HStack, Divider, VStack, Button, Box, Text } from "@chakra-ui/react";
-import { FaGithub, FaComment } from "react-icons/fa";
-import { SiNaver } from "react-icons/si";
+import {HStack, Divider, VStack, Button, Box, Text} from '@chakra-ui/react';
+import {FaGithub, FaComment} from 'react-icons/fa';
+import {SiNaver} from 'react-icons/si';
 
 export default function SocialLogin() {
   const kakaoParams = {
-    response_type: "code",
-    client_id: "564d95aa68dfb025d4f3726ecaac2764",
-    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: 'code',
+    client_id: '564d95aa68dfb025d4f3726ecaac2764',
+    redirect_uri: 'https://mokugyo.biz/social/kakao',
   };
   const kakaoparams = new URLSearchParams(kakaoParams).toString();
   const naverParams = {
-    response_type: "code",
-    client_id: "2703L1b20ifbv5ml0jVy",
-    redirect_uri: "http://127.0.0.1:3000/social/naver",
+    response_type: 'code',
+    client_id: '2703L1b20ifbv5ml0jVy',
+    redirect_uri: 'https://mokugyo.biz/social/naver',
   };
   const naverparams = new URLSearchParams(naverParams).toString();
   return (
     <Box mb="4">
       <HStack my={8}>
         <Divider />
-        <Text textTransform={"uppercase"} color="gray" fontSize={"xs"} as="b">
+        <Text textTransform={'uppercase'} color="gray" fontSize={'xs'} as="b">
           Or
         </Text>
         <Divider />
@@ -34,7 +34,7 @@ export default function SocialLogin() {
           Continue with Github
         </Button>
         <Button
-          as={"a"}
+          as={'a'}
           href={`https://kauth.kakao.com/oauth/authorize?${kakaoparams}`}
           w="100%"
           leftIcon={<FaComment />}
@@ -43,9 +43,9 @@ export default function SocialLogin() {
           Continue with Kakao
         </Button>
         <Button
-          as={"a"}
+          as={'a'}
           href={`https://nid.naver.com/oauth2.0/authorize?${naverparams}`}
-          w={"100%"}
+          w={'100%'}
           leftIcon={<SiNaver />}
           colorScheme="green"
         >
